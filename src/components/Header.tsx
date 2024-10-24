af2c1ae60df11880ea89fb3b12a2c0ca82eb38cf
Kirill Inoz <63226427+kirillinoz@users.noreply.github.com>
2024-10-24 11:32:01 +0200

    Navigation changed :sparkles:

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,28 @@
+'use client';
 import Image from 'next/image';
 import logo from '../../public/logo.png'; // Adjust the path as necessary
 import Link from 'next/link';
+import { LucideHouse } from 'lucide-react';
+import { usePathname } from 'next/navigation';
 
 const Header: React.FC = () => {
+  const pathname = usePathname();
+  const isActive = (href: string) => pathname === href;
+
   return (
     <header className="flex justify-between items-center px-8 py-4 bg-white text-black shadow-lg">
       <div className="flex items-center">
         <Image src={logo} alt="Logo" width={50} height={50} />
-        <p className="ml-3">MaxBox</p>
+        <p className="ml-3 font-bold">MaxBox</p>
       </div>
       <div className="flex-1 flex justify-end">
         <nav className="flex space-x-4">
-          <Link href="#dokumentation">Dokumentation</Link>
-          <Link href="#einstellungen">Einstellungen</Link>
+          <Link href="/">
+            <LucideHouse
+              className={`${isActive('/') && 'opacity-70'} hover:opacity-70`}
+              strokeWidth={3}
+            />
+          </Link>
         </nav>
       </div>
     </header>
